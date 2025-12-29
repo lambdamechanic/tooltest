f10fcc5e637c4fa29de90dd536b5404fb442fd33
Mark Wotton <mwotton@gmail.com>
2025-12-29 11:53:22 +0700

    Default CLI log level to info

--- tooltest-cli/src/main.rs
+++ tooltest-cli/src/main.rs
@@ -5,7 +5,7 @@ use tooltest_cli::{run, Cli};
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> ExitCode {
-    env_logger::Builder::from_env(env_logger::Env::default())
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
         .target(env_logger::Target::Stderr)
         .init();
     let cli = Cli::parse();
